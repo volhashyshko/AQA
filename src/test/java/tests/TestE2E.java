6a095a850b3090e2e988700bd09f3b06fd538048
volhashyshko <olga.shyshko25@gmail.com>
2024-03-06 15:47:56 +0300

    L5_2

--- src/test/java/tests/TestE2E.java
+++ src/test/java/tests/TestE2E.java
@@ -1,23 +1,47 @@
 package tests;
 
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import utils.Driver;
+import utils.Log;
 import java.util.concurrent.TimeUnit;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestE2E {
-    public static void main(String[] args) throws InterruptedException {
+        public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Webdriver\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.name("password")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("input[value='Login']")).click();
+        @BeforeAll
+            public void setUp(){
 
+    Log.info("Открываем начальную страницу");
+    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+    driver.get("https://www.saucedemo.com/");
+    driver.findElement(By.id("user-name")).sendKeys("standard_user");
+    driver.findElement(By.name("password")).sendKeys("secret_sauce");
+    driver.findElement(By.cssSelector("input[value='Login']")).click();
+    Log.info("Проверяем, что открыта корректная страница saucedemo");
+    String pageTitle = driver.getTitle();
+    Assertions.assertEquals("Swag Labs ", pageTitle, "Открыта не Swag Labs или название страницы неверно");
+}
+
+    //public static void main(String[] args) throws InterruptedException {
+        //System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Webdriver\\chromedriver-win64\\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.get("https://www.saucedemo.com/");
+        //driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        //driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        //driver.findElement(By.cssSelector("input[value='Login']")).click();
+
+    @BeforeEach
+            Log.info("Открываем начальную страницу");
+            @Test
+    @DisplayName("")
         driver.findElement(By.xpath("//select[@class='product_sort_container']")).click();
         driver.findElement(By.xpath("//option[@value='lohi']")).click();
         WebElement buttonAdd = driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory '][1]"));
