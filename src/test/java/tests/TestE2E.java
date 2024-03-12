0f43f8c8bde6ced19f437e8843a75e256593cdf4
volhashyshko <olga.shyshko25@gmail.com>
2024-03-12 23:29:29 +0300

    L5_3

--- src/test/java/tests/TestE2E.java
+++ src/test/java/tests/TestE2E.java
@@ -4,49 +4,44 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import utils.Driver;
-import utils.Log;
+
 import java.util.concurrent.TimeUnit;
+
+
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestE2E {
-        public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Webdriver\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+    private final WebDriver driver = Driver.getChromeDriver();
 
-        @BeforeAll
-            public void setUp(){
-
-    Log.info("Открываем начальную страницу");
-    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-    driver.get("https://www.saucedemo.com/");
-    driver.findElement(By.id("user-name")).sendKeys("standard_user");
-    driver.findElement(By.name("password")).sendKeys("secret_sauce");
-    driver.findElement(By.cssSelector("input[value='Login']")).click();
-    Log.info("Проверяем, что открыта корректная страница saucedemo");
-    String pageTitle = driver.getTitle();
-    Assertions.assertEquals("Swag Labs ", pageTitle, "Открыта не Swag Labs или название страницы неверно");
-}
-
-    //public static void main(String[] args) throws InterruptedException {
-        //System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Webdriver\\chromedriver-win64\\chromedriver.exe");
-        //WebDriver driver = new ChromeDriver();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //driver.get("https://www.saucedemo.com/");
-        //driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        //driver.findElement(By.name("password")).sendKeys("secret_sauce");
-        //driver.findElement(By.cssSelector("input[value='Login']")).click();
+    @BeforeAll
+    public void setUp(){
+        System.out.println("Начальная страница открыта");
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("input[value='Login']")).click();
+        System.out.println("Корректная страница saucedemo открыта");
+        String pageTitle = driver.getTitle();
+        Assertions.assertEquals("Swag Labs", pageTitle, "Открыта не Swag Labs или название страницы неверно");
+    }
 
     @BeforeEach
-            Log.info("Открываем начальную страницу");
-            @Test
-    @DisplayName("")
+    public void beforeEachTestMethod() {
+        System.out.println("Некоторый код выполняется перед каждым методом - тест beforeEach");
+
+    }
+    @Test
+    @DisplayName("Тест который проверяет е2е сценарий")
+    public void e2eScenario() throws InterruptedException {
         driver.findElement(By.xpath("//select[@class='product_sort_container']")).click();
         driver.findElement(By.xpath("//option[@value='lohi']")).click();
         WebElement buttonAdd = driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory '][1]"));
         String buttonAddName = buttonAdd.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']")).getText();
+        System.out.println("Проверка возможности добавить товар в корзину");
         System.out.println(buttonAddName.equals("Add to cart"));
+
 
         driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory '][1]")).click();
         driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
@@ -61,18 +56,23 @@ public class TestE2E {
 
         WebElement textSuccess = driver.findElement(By.xpath("//h2[@class='complete-header']"));
         String buttonName = textSuccess.findElement(By.xpath("//h2[@class='complete-header']")).getText();
+        System.out.println("Проверка успешности заказа товара");
         System.out.println(buttonName.equals("Thank you for your order!"));
 
+
         driver.findElement(By.xpath("//button[@name='back-to-products']")).click();
+        System.out.println("Осуществлен возврат на главную страницу");
 
         driver.findElement(By.id("react-burger-menu-btn")).click();
 
         driver.findElement(By.id("logout_sidebar_link")).click();
+        System.out.println("Осуществлен выход из приложения");
 
-        Thread.sleep(5000);
-
-
-        driver.close();
+        Thread.sleep(1000);
+    }
+    @AfterAll
+    public void quit(){
+        System.out.println("Браузер закрыт");
         driver.quit();
 
     }
