f6e2ffd32f58f6b0561c8e589a0bf0edadaa31f7
volhashyshko <olga.shyshko25@gmail.com>
2024-03-13 17:41:38 +0300

    L5_5

--- src/test/java/tests/TestLesson5.java
+++ src/test/java/tests/TestLesson5.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.Driver;
-
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -22,18 +20,14 @@ public class TestLesson5 {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.name("password")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("input[value='Login']")).click();
-        System.out.println("Корректная страница saucedemo открыта");
+        System.out.println("Осуществлен переход на главную страницу");
         String pageTitle = driver.getTitle();
         Assertions.assertEquals("Swag Labs", pageTitle, "Открыта не Swag Labs или название страницы неверно");
+
         //--Нет категорий, проверим сколько всего товаров на странице
-        List<WebElement> inventoryItems = driver.findElements(By.xpath("//div[@class='inventory_item']"));
-        System.out.println("Количество товаров на странице: " + driver.findElements(By.xpath("//div[@class='inventory_item']")).stream().count());
+        System.out.println("Количество товаров на странице: " + (long) driver.findElements(By.xpath("//div[@class='inventory_item']")).size());
     }
 
-    @BeforeEach
-    public void beforeEachTestMethod() {
-        System.out.println("Некоторый код выполняется перед каждым методом - тест beforeEach");
-    }
     @Test
     @DisplayName("Тест который проверяет сценарий задания Lesson 5")
     public void e2eScenario() throws InterruptedException {
@@ -42,11 +36,9 @@ public class TestLesson5 {
         driver.findElement(By.xpath("//select[@class='product_sort_container']")).click();
         driver.findElement(By.xpath("//option[@value='az']")).click();
 
-        WebElement FirstProduct = driver.findElement(By.xpath("//a[@id='item_2_title_link']"));
-        String nameFirstProduct = FirstProduct.findElement(By.xpath("//a[@id='item_2_title_link']")).getText();
-        System.out.println("Название первого товара: " + driver.findElement(By.xpath("//a[@id='item_2_title_link']")).getText());
+        System.out.println("Название первого товара: " + driver.findElement(By.xpath("//a[@id='item_4_title_link']")).getText());
 
-        WebElement buttonAddToCardFirst = driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory '][1]"));
+        WebElement buttonAddToCardFirst = driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']"));
         String buttonAddNameFirst = buttonAddToCardFirst.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']")).getText();
         System.out.println("Проверка возможности добавить товар 1 в корзину");
         System.out.println(buttonAddNameFirst.equals("Add to cart"));
@@ -59,11 +51,9 @@ public class TestLesson5 {
         driver.findElement(By.xpath("//select[@class='product_sort_container']")).click();
         driver.findElement(By.xpath("//option[@value='hilo']")).click();
 
-        WebElement SecondProduct = driver.findElement(By.xpath("//a[@id='item_5_title_link']"));
-        String nameSecondProduct = SecondProduct.findElement(By.xpath("//a[@id='item_5_title_link']")).getText();
         System.out.println("Название второго товара: " + driver.findElement(By.xpath("//a[@id='item_5_title_link']")).getText());
 
-        WebElement buttonAddToCardSecond = driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory '][1]"));
+        WebElement buttonAddToCardSecond = driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']"));
         String buttonAddNameSecond = buttonAddToCardSecond.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']")).getText();
         System.out.println("Проверка возможности добавить товар 2 в корзину");
         System.out.println(buttonAddNameSecond.equals("Add to cart"));
@@ -75,61 +65,53 @@ public class TestLesson5 {
         driver.findElement(By.xpath("//select[@class='product_sort_container']")).click();
         driver.findElement(By.xpath("//option[@value='za']")).click();
 
-        WebElement ThirdProduct = driver.findElement(By.xpath("//a[@id='item_1_title_link']"));
-        String nameThirdProduct = ThirdProduct.findElement(By.xpath("//a[@id='item_1_title_link']")).getText();
-        System.out.println("Название третьего товара: " + driver.findElement(By.xpath("//a[@id='item_1_title_link']")).getText());
+        System.out.println("Название третьего товара: " + driver.findElement(By.xpath("//a[@id='item_3_title_link']")).getText());
 
-        WebElement buttonAddToCardThird = driver.findElement(By.xpath("//*[@id='add-to-cart-sauce-labs-bolt-t-shirt']"));
-        String buttonAddNameThird = buttonAddToCardThird.findElement(By.xpath("//*[@id='add-to-cart-sauce-labs-bolt-t-shirt']")).getText();
+        WebElement buttonAddToCardThird = driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']"));
+        String buttonAddNameThird = buttonAddToCardThird.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']")).getText();
         System.out.println("Проверка возможности добавить товар 3 в корзину");
         System.out.println(buttonAddNameThird.equals("Add to cart"));
-        driver.findElement(By.xpath("//*[@id='add-to-cart-sauce-labs-bolt-t-shirt']")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']")).click();
         System.out.println("Товар 3 добавлен в корзину");
         System.out.println("--------------------------");
 
         //--переход в корзину
         driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
         System.out.println("Переход в корзину успешен");
+
         //--проверим сколько всего товаров в корзине
-        List<WebElement> inventoryItems = driver.findElements(By.xpath("//div[@class='cart_item_label']"));
-        System.out.println("Количество товаров в корзине: " + driver.findElements(By.xpath("//div[@class='cart_item_label']")).stream().count());
+        System.out.println("Количество товаров в корзине: " + (long) driver.findElements(By.xpath("//div[@class='cart_item_label']")).size());
 
         //--проверка, что именно выбранные вещи добавлены в корзину
-        WebElement FirstProductToCart = driver.findElement(By.xpath("//a[@id='item_1_title_link']"));
-        String nameFirstProductToCart = FirstProductToCart.findElement(By.xpath("//a[@id='item_1_title_link']")).getText();
-        System.out.println(FirstProductToCart.equals("Add to cart"));
-        WebElement SecondProductToCart = driver.findElement(By.xpath("//a[@id='item_1_title_link']"));
-        String nameSecondProductToCart = SecondProductToCart.findElement(By.xpath("//a[@id='item_1_title_link']")).getText();
-        System.out.println(SecondProductToCart.equals("Add to cart"));
-        WebElement ThirdProductToCart = driver.findElement(By.xpath("//a[@id='item_1_title_link']"));
-        String nameThirdProductToCart = ThirdProductToCart.findElement(By.xpath("//a[@id='item_1_title_link']")).getText();
-        System.out.println(ThirdProductToCart.equals("Add to cart"));
-        System.out.println("Названия товаров совпадают с выбранными в каталоге");
+        System.out.println("Проверка соответствия товаров в корзине");
+        WebElement FirstProductToCart = driver.findElement(By.xpath("//a[@id='item_4_title_link']"));
+        String nameFirstProductToCart = FirstProductToCart.findElement(By.xpath("//a[@id='item_4_title_link']")).getText();
+        System.out.println(nameFirstProductToCart.equals("Sauce Labs Backpack"));
+        Assertions.assertEquals("Sauce Labs Backpack", nameFirstProductToCart, "Товар в корзине не найден");
 
+        WebElement SecondProductToCart = driver.findElement(By.xpath("//a[@id='item_5_title_link']"));
+        String nameSecondProductToCart = SecondProductToCart.findElement(By.xpath("//a[@id='item_5_title_link']")).getText();
+        System.out.println(nameSecondProductToCart.equals("Sauce Labs Fleece Jacket"));
+        Assertions.assertEquals("Sauce Labs Fleece Jacket", nameSecondProductToCart, "Товар в корзине не найден");
 
+        WebElement ThirdProductToCart = driver.findElement(By.xpath("//a[@id='item_3_title_link']"));
+        String nameThirdProductToCart = ThirdProductToCart.findElement(By.xpath("//a[@id='item_3_title_link']")).getText();
+        System.out.println(nameThirdProductToCart.equals("Test.allTheThings() T-Shirt (Red)"));
+        Assertions.assertEquals("Test.allTheThings() T-Shirt (Red)", nameThirdProductToCart, "Товар в корзине не найден");
 
-        driver.findElement(By.xpath("//button[@name='checkout']")).click();
+        //--удаление товара из корзины
+        driver.findElement(By.xpath("//a[@id='item_4_title_link']")).getText();
+        System.out.println("Название удаляемого товара: " + driver.findElement(By.xpath("//a[@id='item_4_title_link']")).getText());
 
-        driver.findElement(By.id("first-name")).sendKeys("Ol");
-        driver.findElement(By.id("last-name")).sendKeys("Shi");
-        driver.findElement(By.id("postal-code")).sendKeys("00123");
-        driver.findElement(By.xpath("//input[@name='continue']")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn_secondary btn_small cart_button']")).click();
 
-        driver.findElement(By.xpath("//button[@name='finish']")).click();
+        System.out.println("Один товар удален");
+        System.out.println("Количество товаров в корзине: " + (long) driver.findElements(By.xpath("//div[@class='cart_item_label']")).size());
 
-        WebElement textSuccess = driver.findElement(By.xpath("//h2[@class='complete-header']"));
-        String buttonName = textSuccess.findElement(By.xpath("//h2[@class='complete-header']")).getText();
-        System.out.println("Проверка успешности заказа товара");
-        System.out.println(buttonName.equals("Thank you for your order!"));
-
-
-        driver.findElement(By.xpath("//button[@name='back-to-products']")).click();
+        driver.findElement(By.xpath("//button[@class='btn btn_secondary back btn_medium']")).click();
         System.out.println("Осуществлен возврат на главную страницу");
 
-        driver.findElement(By.id("react-burger-menu-btn")).click();
-
-        driver.findElement(By.id("logout_sidebar_link")).click();
-        System.out.println("Осуществлен выход из приложения");
+        System.out.println("Количество доступных к заказу товаров: " + (long) driver.findElements(By.xpath("//button[@class='btn btn_primary btn_small btn_inventory ']")).size());
 
         Thread.sleep(1000);
     }
@@ -137,6 +119,5 @@ public class TestLesson5 {
     public void quit(){
         driver.quit();
         System.out.println("Браузер закрыт");
-
     }
 }
