d3a04123cb9550565fcd1c09fb380551278cfe09
volhashyshko <olga.shyshko25@gmail.com>
2024-03-13 21:54:27 +0300

    L5_6

--- src/main/java/utils/Driver.java
+++ src/main/java/utils/Driver.java
@@ -1,9 +1,9 @@
 package utils;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,7 +11,7 @@ public class Driver {
     private static WebDriver driver;
 
     private static void initializeChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Webdriver\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
     }
 
     public static WebDriver getChromeDriver() {
